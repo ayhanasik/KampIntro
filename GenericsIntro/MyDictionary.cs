793e8e41a94364eb5e19abe18d621327a6c991a1
Ayhan Aşık <ayhan.asik@outlook.com>
2021-01-30 20:50:03 +0300

    Generic list and dictionary

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keys;
+        T2[] values;
+        public MyDictionary()
+        {
+            
+            keys = new T1[0];
+            values = new T2[0];
+        }
+        public void Add(T1 item1,T2 item2)
+        {
+            T1[] tempkeys = keys;
+            T2[] tempvalues = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+            for (int i = 0; i < tempkeys.Length; i++)
+            {
+                keys[i] = tempkeys[i];
+                values[i] = tempvalues[i];
+            }
+            keys[keys.Length-1] = item1;
+            values[values.Length-1]= item2;
+        }
+        public int Count()
+        {
+            return keys.Length;
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("American Express");
+            isimler.Add("VISA");
+            isimler.Add("MasterCard");
+            isimler.Add("TROY");
+
+            Console.WriteLine(isimler.Count() + " elemanlı isimler sınıfı oluşturuldu!");
+            Console.WriteLine("---------------------------");
+            
+            
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("3", "American Express");
+            myDictionary.Add("4", "VISA");
+            myDictionary.Add("5", "MasterCard"); 
+            myDictionary.Add("9", "TROY");
+            Console.WriteLine(myDictionary.Count() + " elemanlı myDictionary sınıfı oluşturuldu!");
+            Console.WriteLine("---------------------------");
+            
+        }
+    }
+}
